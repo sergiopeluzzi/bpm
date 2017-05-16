4357ee09049a7ecd0a61eebf73d34019aa734b8b
rafael.mundel <rafael@poptroco.com.br>
2017-05-16 10:54:19 -0400

    Form builder campo obrigatorio adicionado

--- resources/views/app/rotinas/campos.blade.php
+++ resources/views/app/rotinas/campos.blade.php
@@ -57,27 +57,43 @@
   </div>
 </div>
 <div class="row">
-  <div class="col-sm-6">
+  <div class="col-sm-4">
     <div class="form-group">
       <div class="input-group">
       <input type="text" class="form-control" id="textoAjuda" name="textoAjuda[]" value="" placeholder="Texto de ajuda">
     </div>
     </div>
   </div>
-  <div class="col-sm-6">
+  <div class="col-sm-5">
     <div class="form-group">
       <div class="input-group">
       <input type="text" class="form-control" id="opcaoCampo" name="opcaoCampo[]" value="" placeholder="Opções (separados por vírgula)">
-      <div class="input-group-btn">
-        <button class="btn btn-success" type="button"  onclick="novoCampo();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
-      </div>
+
     </div>
     </div>
   </div>
+  <div class="col-sm-3">
+    <div class="form-group">
+      <div class="input-group">
+        <div class="checkbox">
+        	<input id="signup_v2-agree" name="signup_v2[agree]" data-validation="[NOTEMPTY]" data-validation-message="You must agree the terms and conditions" type="checkbox">
+        		<label for="signup_v2-agree">Campo obrigatório</label>
+        </div>
+                          <div class="input-group-btn">
+                            <button class="btn btn-success" type="button"  onclick="novoCampo();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
+                          </div>
+    </div>
+    </div>
+  </div>
+
+
+
+
 </div>
 
   <div class="clear"></div>
   <div class="row p-r">
+    <br>
   <fieldset class="form-group pull-right">
 									<button type="submit" class="btn">Criar campos</button>
 								</fieldset>
@@ -91,18 +107,16 @@
 
 @section('scripts')
 <script>
-var room = 1;
+var count = 1;
 function novoCampo() {
 
-    room++;
+    count++;
     var objTo = document.getElementById('novoCampo')
     var divtest = document.createElement("div");
-	divtest.setAttribute("class", "form-group removerCampo"+room);
-	var rdiv = 'removerCampo'+room;
-    /*divtest.innerHTML = '<div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="rotuloCampo" name="rotuloCampo[]" value="" placeholder="School name"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="descricaoCampo" name="descricaoCampo[]" value="" placeholder="descricaoCampo"></div></div><div class="col-sm-3 nopadding"><div class="form-group"> <input type="text" class="form-control" id="textoAjuda" name="textoAjuda[]" value="" placeholder="textoAjuda"></div></div><div class="col-sm-3 nopadding"><div class="form-group"><div class="input-group"> <select class="form-control" id="tipoCampo" name="tipoCampo[]"><option value="">Date</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option> </select><div class="input-group-btn"> <button class="btn btn-danger" type="button" onclick="deletarCampos('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div><div class="clear"></div>';*/
+	divtest.setAttribute("class", "form-group removerCampo"+count);
+	var rdiv = 'removerCampo'+count;
 
-    divtest.innerHTML = '<div class="row"><div class="col-sm-4"><div class="form-group"><div class="input-group"><select class="form-control" id="tipoCampo" name="tipoCampo[]"><option value="">Tipo do campo</option><option value="1">Texto</option><option value="2">Área de texto</option><option value="3">Campo de seleção</option><option value="4">Checklist</option><option value="5">Radio</option><option value="6">Número</option><option value="7">Upload</option></select></div></div></div><div class="col-sm-4"><div class="form-group"><input type="text" class="form-control" id="rotuloCampo" name="rotuloCampo[]" value="" placeholder="Rótulo do Campo"></div></div><div class="col-sm-4"><div class="form-group"><input type="text" class="form-control" id="descricaoCampo" name="descricaoCampo[]" value="" placeholder="Descrição"></div></div></div><div class="row"><div class="col-sm-6"><div class="form-group"><div class="input-group"><input type="text" class="form-control" id="textoAjuda" name="textoAjuda[]" value="" placeholder="Texto de ajuda"></div></div></div><div class="col-sm-6"><div class="form-group"><div class="input-group"><input type="text" class="form-control" id="opcaoCampo" name="opcaoCampo[]" value="" placeholder="Opções (separados por vírgula)"><div class="input-group-btn">  <button class="btn btn-danger" type="button"  onclick="deletarCampos('+ room +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button></div></div></div></div></div>';
-
+     divtest.innerHTML = '<div class="row"> <div class="col-sm-4"> <div class="form-group"> <div class="input-group"> <select class="form-control" id="tipoCampo" name="tipoCampo[]"> <option value="">Tipo do campo</option> <option value="1">Texto</option> <option value="2">Área de texto</option> <option value="3">Campo de seleção</option> <option value="4">Checklist</option> <option value="5">Radio</option> <option value="6">Número</option> <option value="7">Upload</option> </select> </div> </div> </div> <div class="col-sm-4"> <div class="form-group"> <input type="text" class="form-control" id="rotuloCampo" name="rotuloCampo[]" value="" placeholder="Rótulo do Campo"> </div> </div> <div class="col-sm-4"> <div class="form-group"> <input type="text" class="form-control" id="descricaoCampo" name="descricaoCampo[]" value="" placeholder="Descrição"> </div> </div> </div> <div class="row"> <div class="col-sm-4"> <div class="form-group"> <div class="input-group"> <input type="text" class="form-control" id="textoAjuda" name="textoAjuda[]" value="" placeholder="Texto de ajuda"> </div> </div> </div> <div class="col-sm-5"> <div class="form-group"> <div class="input-group"> <input type="text" class="form-control" id="opcaoCampo" name="opcaoCampo[]" value="" placeholder="Opções (separados por vírgula)"> </div> </div> </div> <div class="col-sm-3"> <div class="form-group"> <div class="input-group"> <div class="checkbox"> <input id="signup_v2-agree" name="signup_v2[agree]" data-validation="[NOTEMPTY]" data-validation-message="You must agree the terms and conditions" type="checkbox"> <label for="signup_v2-agree">Campo obrigatório</label> </div> <div class="input-group-btn"> <button class="btn btn-danger" type="button"  onclick="deletarCampos('+ count +');"> <span class="glyphicon glyphicon-minus" aria-hidden="true"></span> </button> </div> </div> </div> </div> </div>';
 
     objTo.appendChild(divtest)
 }
